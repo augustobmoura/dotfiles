dbf342dd4adf43cd79a15fe982152eb95e41d4bc
Augusto Moura <augusto.borgesm@gmail.com>
2019-06-19 19:45:21 -0300

    first commit

---
+++ ln-setup.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env sh
+
+ln -s "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+ln -s "$HOME/dotfiles/oh-my-zsh" "$HOME/.oh-my-zsh"
+ln -s "$HOME/dotfiles/functions" "$HOME/functions"
+ln -s "$HOME/dotfiles/tmux-theme/.tmux.conf" "$HOME/.tmux.conf"
+ln -s "$HOME/dotfiles/.tmux.conf.local" "$HOME/.tmux.conf"
+ln -s "$HOME/dotfiles/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" "$HOME/highlighting.zsh"
+ln -s "$HOME/dotfiles/zsh-autosuggestions/zsh-autosuggestions.zsh" "$HOME/autosuggestion.zsh"
+

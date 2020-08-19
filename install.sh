92417fd5a6018122d135f4a71532ef4f23c04abb
Augusto Moura <augusto.borgesm@gmail.com>
2020-08-19 16:48:16 -0300

    configure git for the dotfile project on install

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-#! /usr/bin/env sh
+#! zsh
 
 set -eu
 
 export DOTFILES_HOME="${DOTFILES_HOME:-"$HOME/dotfiles"}"
+
+if type git &> /dev/null; then
+	(
+		cd "$DOTFILES_HOME"
+		git config user.email augusto.borgesm@gmail.com
+		git config user.name "Augusto Moura"
+	)
+fi
 
 yes n | "$DOTFILES_HOME/ln-files.sh"
 

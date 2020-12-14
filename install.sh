738083c6c22159a2b8b0a23472a35e12fca3eb0f
Augusto Moura <augusto.borgesm@gmail.com>
2020-12-14 16:26:06 -0300

    added mons as a third-party script

--- install.sh
+++ install.sh
@@ -7,11 +7,20 @@ export DOTFILES_HOME="${DOTFILES_HOME:-"$HOME/dotfiles"}"
 if type git &> /dev/null; then
 	(
 		cd "$DOTFILES_HOME"
+
 		git config user.email augusto.borgesm@gmail.com
 		git config user.name "Augusto Moura"
+
+		git submodule update --init --recursive
 	)
 fi
 
 yes n | "$DOTFILES_HOME/ln-files.sh"
 
 yes y | "$DOTFILES_HOME/third-party/fzf/install"
+
+# Install mons
+(
+	cd "$DOTFILES_HOME/third-party/mons"
+	make -e PREFIX="$PWD/.build" install
+)
